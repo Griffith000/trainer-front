838eba2e967ccb3a4c47b5e177a10ccc456e9f09
Griffith000 <latrechghassen68@gmail.com>
2026-03-02 17:08:44 +0100

    feat : add userProfile type

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -3,3 +3,32 @@ export interface User {
   email: string;
   onboarding_completed: boolean;
 }
+
+export interface UserProfile {
+  full_name: string;
+  date_of_birth: string;
+  gender: "male" | "female" | "prefer_not_to_say";
+  current_weight_kg: string;
+  height_cm: string;
+  target_weight_kg: string;
+  fitness_goal:
+    | "lose_weight"
+    | "build_muscle"
+    | "improve_endurance"
+    | "general_wellness";
+  activity_level:
+    | "sedentary"
+    | "lightly_active"
+    | "moderately_active"
+    | "very_active";
+  dietary_preferences: (
+    | "vegetarian"
+    | "vegan"
+    | "gluten_free"
+    | "no_restrictions"
+  )[];
+  dietary_other_text: string;
+  health_notes: string;
+  disclaimer_accepted_at: string;
+  updated_at: string;
+}
